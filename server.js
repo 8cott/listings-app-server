fb7806d5a5e23cc8a051fdf4b2ca6998fd4d0617
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 16:42:25 -0400

    deployment

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ app.use((req, res, next) => {
   
   res.setHeader('Access-Control-Allow-Origin', allowOrigin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
