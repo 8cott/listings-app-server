8c4d7bb1b0915970d355097037d93f8f2b01a21d
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 15:53:23 -0400

    deployment

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  const allowOrigin = process.env.NODE_ENV === 'production'
+    ? 'https://sr-listings-app-48514e312801.herokuapp.com'
+    : 'http://localhost:5173';
+  
+  res.setHeader('Access-Control-Allow-Origin', allowOrigin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);   
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
@@ -40,20 +45,22 @@ app.use('/listings', listingsController);
 if (process.env.NODE_ENV === 'production') {
   // Serve static files from the React app
   app.use(express.static(path.join(__dirname, '../client/dist')));
-
+  
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-} else {
-  app.use(express.static('public'));
-}
+} // <-- This closing bracket was missing.
 
 // 404 Page
-app.use('*', (req, res) => {
-  res.send('404')
-})  
+app.use((req, res, next) => {
+  if (!req.route) {
+    res.status(404).send('404');
+  } else {
+    next();
+  }
+});
 
 // LISTEN
 app.listen(PORT, () => {
