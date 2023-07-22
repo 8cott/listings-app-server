52caac6f1464141db633e92ccebc59804759a3c5
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 20:44:40 -0400

    deployment

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const methodOverride = require('method-override')
 const mongoose = require('./config/db')
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/AuthRoute');
-const path = require('path');
-const cors = require('cors');
 
 // CONFIGURATION
 require('dotenv').config({ path: './.env' })
@@ -13,47 +11,35 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(cookieParser());
 
 // Enable CORS
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);   
+  next();
+});
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.redirect('/listings');
 });
-
 app.use('/', authRoute);
 
 // LISTINGS
 const listingsController = require('./controllers/listings_controller.js');
 app.use('/listings', listingsController);
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  
-  // The "catchall" handler: for any request that doesn't
-  // match one above, send back React's index.html file.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}  
-
 // 404 Page
-app.use((req, res, next) => {
-  if (!req.route) {
-    res.status(404).send('404');
-  } else {
-    next();
-  }
-});
+app.use('*', (req, res) => {
+    res.send('404')
+  })  
 
 // LISTEN
 app.listen(PORT, () => {
