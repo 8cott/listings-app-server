fda26b251aea06310d04efe4ae3e7ea2ea4daec4
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 21:43:16 -0400

    deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override')
 const mongoose = require('./config/db')
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/AuthRoute');
+const path = require('path');
 
 // CONFIGURATION
 require('dotenv').config({ path: './.env' })
@@ -11,7 +12,6 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
-app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
@@ -19,28 +19,39 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);   
   next();
 });
 
-// AUTH ROUTE
+// ROUTES
+app.get('/', (req, res) => {
+  res.redirect('/listings');
+});
 app.use('/', authRoute);
 
 // LISTINGS
 const listingsController = require('./controllers/listings_controller.js');
 app.use('/listings', listingsController);
 
-// HOME ROUTE
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} else {
+  app.use(express.static('public'));
+}
 
 // 404 Page
 app.use('*', (req, res) => {
-    res.status(404).send('404');
+  res.send('404')
 })  
 
 // LISTEN
