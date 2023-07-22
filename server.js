7068d86b4c75acba9c87f88cd6e41f1bb169a13b
Scott <109820423+8cott@users.noreply.github.com>
2023-07-21 21:35:41 -0400

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // DEPENDENCIES
 const express = require('express')
-const path = require('path');
 const methodOverride = require('method-override')
 const mongoose = require('./config/db')
 const cookieParser = require('cookie-parser');
@@ -12,6 +11,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
@@ -19,27 +19,29 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', req.get('origin'));
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);   
   next();
 });
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// ROUTES
+// AUTH ROUTE
 app.use('/', authRoute);
 
 // LISTINGS
 const listingsController = require('./controllers/listings_controller.js');
 app.use('/listings', listingsController);
 
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+// HOME ROUTE
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
+
+// 404 Page
+app.use('*', (req, res) => {
+    res.status(404).send('404');
+})  
 
 // LISTEN
 app.listen(PORT, () => {
