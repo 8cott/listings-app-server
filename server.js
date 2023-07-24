218eadaa17d5d4d3823db20a9dbad10451659c7d
Scott <109820423+8cott@users.noreply.github.com>
2023-07-24 10:47:04 -0400

    reverted changes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const methodOverride = require('method-override')
 const mongoose = require('./config/db')
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/AuthRoute');
-const path = require('path');
 
 // CONFIGURATION
 require('dotenv').config({ path: './.env' })
@@ -12,6 +11,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);   
@@ -36,23 +36,10 @@ app.use('/', authRoute);
 const listingsController = require('./controllers/listings_controller.js');
 app.use('/listings', listingsController);
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  // The "catchall" handler: for any request that doesn't
-  // match one above, send back React's index.html file.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-} else {
-  app.use(express.static('public'));
-}
-
 // 404 Page
 app.use('*', (req, res) => {
-  res.send('404')
-})  
+    res.send('404')
+  })  
 
 // LISTEN
 app.listen(PORT, () => {
